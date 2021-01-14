8d8a3fbdd3547daed7816240b91c6e4deebdad4f
Dolores Argandoña <dolores.ar.pei@gmail.com>
2021-01-14 10:23:35 +0100

    CODIGO FINAL CON JAVDOC DNI 20836996P

--- src/empleadoed_20836996p/EmpleadoED_20836996P.java
+++ src/empleadoed_20836996p/EmpleadoED_20836996P.java
@@ -4,22 +4,33 @@
  * and open the template in the editor.
  */
 package empleadoed_20836996p;
-
 /**
- *
- * @author Administrator
+ *Ejercicio práctico examen EVAL 1 ED
+ * 
+ * @author Dolores Argandoña DNI 20836996P
+ * @version 1.0
  */
 public class EmpleadoED_20836996P {
-
    static final double FACTOR_HORA_EXTRA = 1.2;
     private String nombreCompleto, dni;
-    private double salarioBasePorHora = 10.56;
-
+    private double salarioBasePorHora = 10.56;  
+    /**
+     * constructor con tres parámetros
+     * 
+     * @param nombreCompleto nombre completo en mayúsculas 
+     * @param dni documento nacional de identidad con letra y sin espacio
+     */
     public EmpleadoED_20836996P(String nombreCompleto, String dni) {
         this.nombreCompleto = nombreCompleto;
         this.dni = dni;
     }
-
+    /**
+     * CAlcula el salario bruto mensual en función del salario base por hora y la shoras extra
+     * el precio por hora de la hora extra se determina con la constante FACOR_HORA_EXTRA
+     * 
+     * @param horasExtra numero de horas extra dedicadas redondeado a la baja (entero)
+     * @return devuelve el salario bruto mensual en euros con lso deciomales
+     */
     public double getSalarioEsteMes(int horasExtra) {
         double cantidadExtra, salarioFinal;
         cantidadExtra = horasExtra * this.salarioBasePorHora * FACTOR_HORA_EXTRA;
@@ -27,7 +38,6 @@ public class EmpleadoED_20836996P {
         salarioFinal = (double) Math.round(salarioFinal * 100d) / 100d;
         return salarioFinal;
     }
-
     // Este método no lo comentaremos con JavaDoc
     public static void main(String[] args) {
         // !!!! MODIFICA TU NOMBRE Y TU DNI EN LA SIGUIENTE LÍNEA        
